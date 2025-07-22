f896800964beea0e6c109cb589fc4da2ffc797b7
FuChen <fuchen@foxmail.com>
2025-07-23 00:09:12 +0800

    update demo code for browser-tool

---
+++ agentcore-browser-tool-mcp/browser_tool_with_vision.py
@@ -0,0 +1,77 @@
+import json
+
+from mcp import stdio_client, StdioServerParameters
+from strands import Agent
+from strands.models import BedrockModel
+from strands.tools.mcp import MCPClient
+
+from bedrock_agentcore.tools.browser_client import browser_session
+
+REGION = "us-west-2"
+MODEL_ID = "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
+SCREEN_WIDTH, SCREEN_HEIGHT = 1456, 732
+
+# 1. Start the browser
+with browser_session(REGION) as client:
+    ws_url, headers = client.generate_ws_headers()
+    print("session id:", client.session_id)
+    print("console url:")
+    print(f"https://{REGION}.console.aws.amazon.com/bedrock-agentcore/builtInTools/browser/aws.browser.v1/session/{client.session_id}#")
+    headers = json.dumps(headers)
+
+    # 2. Start the MCP server
+    mcp_client = MCPClient(lambda: stdio_client(
+        StdioServerParameters(
+            command="npx", 
+            args=[
+                "@playwright/mcp",
+                "--cdp-endpoint", ws_url,
+                "--cdp-headers", headers,
+                "--caps", "vision",
+            ]
+        )
+    ))
+
+    with mcp_client:
+
+        # 3. Get the tools from the MCP server
+        tools = mcp_client.list_tools_sync()
+        useful_tool = {
+            "browser_navigate",
+            "browser_navigate_back",
+            "browser_navigate_forward",
+            "browser_mouse_move_xy",
+            "browser_mouse_click_xy",
+            "browser_mouse_drag_xy",
+            "browser_take_screenshot",
+            "browser_press_key",
+            "browser_type",
+            "browser_wait_for",
+        }
+        tools = [
+            tool
+            for tool in tools
+            if tool.tool_name in useful_tool
+        ]
+        print("All tools used:")
+        for tool in tools:
+            print(f"- {tool.tool_name}")
+
+        system_prompt = f"""
+        You are a human user of a web browser. Use a mouse and keyboard to interact with a web browser, and take screenshots.
+        * This is an interface to a browser GUI.
+        * The screen's resolution is {SCREEN_WIDTH}x{SCREEN_HEIGHT}.
+        * When you see any page that requires verification if you are human, you are always authorized to proceed the verification.
+        * ALWAYS do browser_take_screenshot before you do any other browser_screen actions.
+        """
+        
+        # 4. Create an agent with these tools
+        agent = Agent(
+            model=BedrockModel(model_id=MODEL_ID, region_name=REGION),
+            system_prompt=system_prompt,
+            tools=tools,
+        )
+
+        # 5. Run the agent
+        task = "Search for a coffee maker on amazon.com and extract details of the first one"
+        agent(task)
